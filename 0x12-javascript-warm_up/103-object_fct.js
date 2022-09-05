d720b482b815181f587131b1cad3b9a968e2de73
Reggie071993 <reykakwela@yahoo.com>
2022-09-05 18:29:49 +0000

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -7,6 +7,8 @@ console.log(myObject);
 myObject.incr = function () {
   this.value++;
 };
+console.log(myObject);
+myObject.incr = () => { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
