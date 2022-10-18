750cec850b8ce3482bd825426bc18419dc494c0d
Reggie071993 <reykakwela@yahoo.com>
2022-10-18 17:24:19 +0000

    Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+const { argv } = require('process');
+
+const BaseUrl = 'https://swapi-api.hbtn.io/api';
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  console.log(JSON.parse(body).title);
 });
