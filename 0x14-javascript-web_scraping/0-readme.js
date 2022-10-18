3593cd2e97cfa5bc9cecdb97aa3e7798db0c181a
Reggie071993 <reykakwela@yahoo.com>
2022-10-18 17:16:58 +0000

    Fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,5 @@
-fs.readFile(file, 'utf-8', function (err, data) {
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
